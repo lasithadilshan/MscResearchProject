41b321180107072914789c0c8aaaed19fb9b7bb0
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2025-01-04 21:23:57 +0530

    Update app.py footer hidden

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ st.set_page_config(
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
             """
