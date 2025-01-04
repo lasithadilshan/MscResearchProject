8f00df8360a3762e54cf37ef7cf2cdc48c1bacad
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2025-01-04 20:53:01 +0530

    Update app.py HIDE STREAMLIT BRANDING

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ st.set_page_config(
     page_icon="images/favicon.png"
 )
 
+# --- HIDE STREAMLIT BRANDING ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # Get the API key from Streamlit secrets
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
