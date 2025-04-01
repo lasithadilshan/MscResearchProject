e8757043dccc57979d7ba50c20e3282145f1a4ea
lasithadilshan <dilshantilakaratne29@gmail.com>
2025-04-01 21:01:17 +0530

    Remove redundant model_rebuild() call for ChatOpenAI in app.py

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 
-# Ensure ChatOpenAI is fully defined
-ChatOpenAI.model_rebuild()
-
 st.set_page_config(
     page_title="SDLC Automate APP",
     page_icon="images/favicon.png"
