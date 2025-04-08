b669743601243c3fa1f2a09b4cefdd2235972f75
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2025-04-08 13:44:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,9 +123,9 @@ with tab1:
             start_query_time = time.time()
             matches = vector_store.similarity_search(prompt_message, k=3)  # Retrieve top 3 similar texts
             llm = ChatOpenAI(
-                api_key=OPENAI_API_KEY,
-                temperature=0.1,
-                model="gpt-4o"
+                model="gpt-4o",
+                temperature=0,
+                api_key=OPENAI_API_KEY
             )
             qa_chain = RetrievalQA.from_chain_type(
                 llm=llm,
